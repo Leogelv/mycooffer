c963d43c92adb834b7d6d37ae9e355861394acd7
Leogelv <monxydd@gmail.com>
2025-04-20 21:27:40 +0700

    Исправлена ошибка переполнения стека при сборке проекта

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 
 const nextConfig = {
   /* config options here */
+  experimental: {
+    // Отключаем сбор трасс сборки для предотвращения ошибки Maximum call stack size exceeded
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/**/*',
+        '.git/**/*',
+        '.next/**/*',
+      ]
+    },
+    outputFileTracingRoot: process.cwd(),
+  },
+  // Оптимизируем изображения
+  images: {
+    unoptimized: true,
+  }
 };
 
 module.exports = nextConfig; 
